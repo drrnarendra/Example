e0f757fd2c258cff200773c5da2b3e1dc60ab033
sam <nareshmahendra@gmail.com>
2017-12-26 11:41:26 +0530

    gg

--- Examples/src/testDB.java
+++ Examples/src/testDB.java
@@ -1,5 +1,12 @@
 
-public class testDB {public testDB() {
+public class testDB {
+	public testDB() {
 	// TODO Auto-generated constructor stub
+	
 }
+	public testDB(String string) {
+	// TODO Auto-generated constructor stub
+	
+}
+
 }
